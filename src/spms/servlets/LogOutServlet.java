cb40f6f56188337b99af8ad6256a9ac774e525bb
unknown <>
2016-01-28 01:51:46 +0900

    logout

---
+++ src/spms/servlets/LogOutServlet.java
@@ -0,0 +1,21 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogOutServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.invalidate();	// session object  무효화
+		
+		response.sendRedirect("login");
+	}
+
+}

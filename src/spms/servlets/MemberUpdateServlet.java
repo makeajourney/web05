52f5fe0b81aacdb533ce80ffdd34050a40be413d
unknown <>
2016-02-12 00:21:55 +0900

    /member/update DAO 적용

--- src/spms/servlets/MemberUpdateServlet.java
+++ src/spms/servlets/MemberUpdateServlet.java
@@ -2,9 +2,6 @@ package spms.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -14,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import spms.dao.MemberDao;
 import spms.vo.Member;
 
 
@@ -29,30 +27,18 @@ public class MemberUpdateServlet extends HttpServlet {
 			HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+
 		try {
 			ServletContext sc = this.getServletContext();
-			Class.forName(sc.getInitParameter("driver"));
 			conn = (Connection) sc.getAttribute("conn");
-			/*
-			conn = DriverManager.getConnection(
-						sc.getInitParameter("url"),
-						sc.getInitParameter("username"),
-						sc.getInitParameter("password"));
-			*/ 
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(
-				"SELECT MNO,EMAIL,MNAME,CRE_DATE FROM MEMBERS" + 
-				" WHERE MNO=" + request.getParameter("no"));	
-			rs.next();
 			
-			Member member = new Member()
-					.setNo(rs.getInt("MNO"))
-					.setName(rs.getString("MNAME"))
-					.setEmail(rs.getString("EMAIL"))
-					.setCreatedDate(rs.getDate("CRE_DATE"));
-			request.setAttribute("member", member);
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+			
+			request.setAttribute("member", 
+					memberDao.selectOne(
+							Integer.parseInt(request.getParameter("no"))
+							));
 			
 			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberUpdateForm.jsp");
 			rd.forward(request, response);
@@ -81,12 +67,7 @@ public class MemberUpdateServlet extends HttpServlet {
 			
 		} catch (Exception e) {
 			throw new ServletException(e);
-			
-		} finally {
-			try {if (rs != null) rs.close();} catch(Exception e) {}
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			// try {if (conn != null) conn.close();} catch(Exception e) {}
-		}
+		} 
 	}
 	
 	@Override
@@ -97,24 +78,18 @@ public class MemberUpdateServlet extends HttpServlet {
 		//request.setCharacterEncoding("UTF-8");
 		
 		Connection conn = null;
-		PreparedStatement stmt = null;
+
 		try {
 			ServletContext sc = this.getServletContext();
-			Class.forName(sc.getInitParameter("driver"));
 			conn = (Connection) sc.getAttribute("conn");
-			/*
-			conn = DriverManager.getConnection(
-						sc.getInitParameter("url"),
-						sc.getInitParameter("username"),
-						sc.getInitParameter("password"));
-			*/ 
-			stmt = conn.prepareStatement(
-					"UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=now()"
-					+ " WHERE MNO=?");
-			stmt.setString(1, request.getParameter("email"));
-			stmt.setString(2, request.getParameter("name"));
-			stmt.setInt(3, Integer.parseInt(request.getParameter("no")));
-			stmt.executeUpdate();
+			
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+			
+			memberDao.update(new Member()
+					.setEmail(request.getParameter("email"))
+					.setName(request.getParameter("name"))
+					.setNo(Integer.parseInt(request.getParameter("no"))));
 			
 			response.sendRedirect("list");
 			
@@ -123,9 +98,6 @@ public class MemberUpdateServlet extends HttpServlet {
 			request.setAttribute("error", e);
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
 			rd.forward(request, response);
-		} finally {
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			// try {if (conn != null) conn.close();} catch(Exception e) {}
 		}
 	}
 }

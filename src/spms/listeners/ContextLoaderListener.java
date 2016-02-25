f61614c94b4840722d94b548d4b411e1557c0977
Soyoun Kim <makeajourney@gmail.com>
2016-02-26 03:25:50 +0900

    Server DataSource 적용

--- src/spms/listeners/ContextLoaderListener.java
+++ src/spms/listeners/ContextLoaderListener.java
@@ -1,20 +1,18 @@
 package spms.listeners;
 
-import java.sql.SQLException;
-
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-import org.apache.commons.dbcp.BasicDataSource;
+import javax.sql.DataSource;
 
 import spms.dao.MemberDao;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener{
 
-	BasicDataSource ds;
+	// BasicDataSource ds;
 	
 	
 	@Override
@@ -23,12 +21,9 @@ public class ContextLoaderListener implements ServletContextListener{
 		try {
 			ServletContext sc = event.getServletContext();
 			
-			ds = new BasicDataSource();
-			ds.setDriverClassName(sc.getInitParameter("driver"));
-			ds.setUrl(sc.getInitParameter("url"));
-			ds.setUsername(sc.getInitParameter("username"));
-			ds.setPassword(sc.getInitParameter("password"));
-
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource) initialContext.lookup("java:comp/env/jdbc/studydb");
+			
 			MemberDao memberDao = new MemberDao();
 			memberDao.setDataSource(ds);
 			
@@ -42,6 +37,5 @@ public class ContextLoaderListener implements ServletContextListener{
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		// 웹애플리케이션이 종료될 때 실행
-		try { if (ds != null) ds.close(); } catch (SQLException e) {}
 	}
 }
